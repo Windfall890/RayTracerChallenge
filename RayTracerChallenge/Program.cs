70e0c9ae5dd64ee6db7b07b490d58d1d51ba921b
Jon Sanders <Jon.Sanders@redbox.com>
2019-05-25 16:35:34 -0500

    Initial commit

---
+++ RayTracerChallenge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RayTracerChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
